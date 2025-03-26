b5e21a5cf2054a67849ef3084cfc142558436b03
Mark Thornber <mark.thornber@merg.org.uk>
2025-03-26 12:55:33 +0000

    Added use of 'public' directory

--- www/vite.config.mjs
+++ www/vite.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-import wasmPack from "vite-plugin-wasm";
+import elmPlugin from "vite-plugin-elm";
 // not needed, but important for supporting older browsers
 import topLevelAwait from "vite-plugin-top-level-await";
-import elmPlugin from "vite-plugin-elm";
+import wasmPack from "vite-plugin-wasm";
 
 export default defineConfig({
     base: "/elm-rust-tutorial/www/dist",
